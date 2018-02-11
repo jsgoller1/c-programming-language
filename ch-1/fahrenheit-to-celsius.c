859dd99865b5458bb8f2aede0cba2308f6b4d20a
Joshua Goller <jsgoller1@gmail.com>
2018-02-11 15:36:36 -0800

    first examples

---
+++ ch-1/fahrenheit-to-celsius.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+   for fahr = 0, 20, ... , 300 */
+
+int main () {
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0; // lower limit of temperature table
+    upper = 300; // upper limit 
+    step = 20; // size increment
+
+    fahr = lower;
+    printf("F\tC\n");
+    printf("-------------\n");   
+    while (fahr <= upper) {
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}

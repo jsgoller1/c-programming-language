a52360a5118e0f4639db2f922d41b3185f004846
Joshua Goller <jsgoller1@gmail.com>
2018-02-11 18:22:02 -0800

    more examples

---
+++ ch-1/examples/line-count.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// count lines in input, ver. 1
+
+int main()
+{
+    int c, n1;
+
+    n1 = 0;
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+            ++n1;
+    printf("%d\n", n1); // Note that this will count newlines as characters!
+    return 0;
+}

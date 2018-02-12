b359891d9acc8f455975fa338d441dd405eb9cba
Joshua Goller <jsgoller1@gmail.com>
2018-02-12 00:41:13 -0800

    1-15.c

---
+++ ch-1/exercises/1-15.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+   for fahr = 0, 20, ... , 300 */
+
+float fahr_to_cels(float fahr);
+float cels_to_fahr(float cels);
+
+float fahr_to_cels(float fahr) {
+    return 5.0/9.0 * ( fahr - 32.0);
+}
+
+float cels_to_fahr(float cels) {
+    return 32.0 + ( cels * (9.0/5.0));
+}
+
+int main () {
+    printf("%f\n", fahr_to_cels(212.0));
+    printf("%f\n", fahr_to_cels(32.0));
+    printf("%f\n", cels_to_fahr(100.0));
+    printf("%f\n", cels_to_fahr(0));
+    return 0;
+}

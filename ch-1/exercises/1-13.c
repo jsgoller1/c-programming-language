4eaf31637dc9a3548d035f64d59bf7e1d64a7cc5
Joshua Goller <jsgoller1@gmail.com>
2018-02-13 00:50:55 -0800

    elliotts challenge

--- ch-1/exercises/1-13.c
+++ ch-1/exercises/1-13.c
@@ -7,42 +7,47 @@
 int main()
 {
     int i, c, word_length, state;
-    int words[11]; // 0 through 9, then 10+
+    int words[10]; // 1 through 9, then 10+
 
     word_length = 0;
     state = OUT;
 
-    for (i = 0; i <= 11; i++)
+    for (i = 0; i < 10; i++)
         words[i] = 0;
 
     while ((c = getchar()) != EOF) {
         if (c == ' ' || c == '\n' || c == '\t') {
             state = OUT;
-            if (word_length >= 10)
-                words[10]++;
-            else
-                words[word_length]++;
-            word_length = 0;
+            if (word_length >= 10) {
+                words[9]++;
+            } else if (word_length >= 1) {
+                words[word_length-1]++;
+                word_length = 0;
+            }   
         } else if (state == OUT)
             state = IN;
         if (state == IN)
             ++word_length;
     }
 
-    /* 
-    printf("Word Lengths contents\n0: %4d\n1: %4d\n2: %4d\n3: %4d\n4: %4d\n5: %4d\n6: %4d\n7: %4d\n8: %4d\n9: %4d\n10: %4d\n\n", 
-            words[0], words[1], words[2], words[3], words[4], words[5], 
-            words[6], words[7], words[8], words[9], words[10]);
-    */
+     
+    printf("Word Lengths contents\n");
+    for (i=0; i<9; i++){
+        printf("%d: %4d\n", i+1, words[i]);
+    }       
 
-    printf("%4d%4d%4d%4d%4d%4d%4d%4d%4d%4d%4d+\n", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    for (i=0; i<9; i++){
+        printf("%4d", i+1);
+    }    
+    printf("%4d+\n", 10);
+
     int curr_column, curr_row, more_rows;
-    char hist_row[11];
+    char hist_row[10];
     curr_column = curr_row = 0;
     more_rows = 1;
     while (more_rows) {
         more_rows = 0;
-        for(curr_column = 0; curr_column <= 10; curr_column++) {
+        for(curr_column = 0; curr_column <= 9; curr_column++) {
             if (words[curr_column] > curr_row) {
                 more_rows = 1;
                 hist_row[curr_column] = 'X';
@@ -53,8 +58,10 @@ int main()
         }
         ++curr_row;
 
-        printf("%4c%4c%4c%4c%4c%4c%4c%4c%4c%4c%4c\n", hist_row[0], hist_row[1], hist_row[2],
-        hist_row[3], hist_row[4], hist_row[5], hist_row[6], hist_row[7], hist_row[8], hist_row[9], hist_row[10]);    
+        for (i=0; i<10; i++){
+            printf("%4c", hist_row[i]);
+        }
+        printf("\n");
     }
    
     return 0;

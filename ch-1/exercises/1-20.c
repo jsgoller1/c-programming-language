62d51f895798565c78bd0e3211fcb8ba6bf7c556
Joshua Goller <jsgoller1@gmail.com>
2018-02-13 23:53:37 -0800

    bugfix for EOF handling

--- ch-1/exercises/1-20.c
+++ ch-1/exercises/1-20.c
@@ -25,24 +25,24 @@ void detab(char string[], int len, int whitespaces){
     char copy[MAXLINE];
     int i, j, k;
     k = 0;
+
     for (i = 0; i < len; i++){
-        if (string[i] == '\t'){
+        if (string[i] == '\t') {
             for (j = 0; j < whitespaces; j++) {
-                //printf("writing %d period\n", j);
                 copy[k] = '.';
                 k++;
             }
         } else {
-            //printf("writing %c to copy\n", string[i]);
             copy[k] = string[i];
             k++;
         }
     }
-    //printf("%d\n", k);
+    
     for (i = 0; i < k; i++){
-        //printf("writing back %c\n", copy[i]);
         string[i] = copy[i];
     }
+
+    string[k] = '\0';
 }
 
 // getline(): read a line into s, return length
@@ -50,12 +50,19 @@ int getline(char s[], int lim)
 {
     int c, i;
 
-    for (i=0; i<lim-1 && (c=getchar()) != EOF && c != '\n'; ++i)
+    for (i=0; (i < lim-1) && ((c=getchar()) != EOF) && (c != '\n'); ++i) {
         s[i] = c;
-    if (c == '\n') {
+    }
+
+    if (c == EOF) {
+        printf("\nGot EOF, quitting immediately.\n");
+        return 0;
+    } else if (c == '\n') {
         s[i] = c;
         ++i;
     }
+
     s[i] = '\0';
+
     return i;
 }

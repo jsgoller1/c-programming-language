5a528d22c37293480a595152d4c73c327b272b0d
Joshua Goller <joshua.goller@uber.com>
2018-02-17 12:04:23 -0800

    strlen example

---
+++ ch-2/examples/2.3-strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int mystrlen(char s[]);
+
+int main(){
+  char name[] = {"Joshua"};
+  printf("%d\n", mystrlen(name));
+  return 0;
+}
+
+int mystrlen(char s[]){
+  int i;
+
+  i = 0;
+  while (s[i] != '\0')
+  {
+    ++i;
+  }
+
+  return i;
+}

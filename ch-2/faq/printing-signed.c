2b76cf38fe662d7d33a0f42e4622118dfd8f3ac1
Joshua Goller <jsgoller1@gmail.com>
2018-02-18 22:52:06 -0800

    printing-signed.c

---
+++ ch-2/faq/printing-signed.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Does the binary representation of 0x80000000 + 0x00000001 change
+// depending on the type each is declared as?
+
+int main() 
+{
+    unsigned int a = 0x80000000;
+    unsigned int b = 0x00000001;
+
+    signed int c = 0x80000000;
+    signed int d = 0x00000001;
+
+    printf("0x%x = unsigned %u\n", a+b, a+b);
+    printf("0x%x = signed %d\n", c+d, c+d);
+
+    return 0;
+}

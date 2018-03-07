7c0f753a2cfb444352209a98e262c1bd8c5ee876
Joshua Goller <joshua.goller@uber.com>
2018-03-07 00:33:07 -0800

    half of 5.1 example

---
+++ ch-5/examples/5.1-getch.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+
+int getch();
+void ungetch(int val);
+int getint(int *pn);
+
+int main()
+{
+
+  return 0;
+}
+
+int getint(int *pn)
+{
+  int c, sign;
+
+  while (isspace(c = getch()))
+  {
+    // no op
+  }
+
+  if (!isdigit(c) && c != EOF && )
+}

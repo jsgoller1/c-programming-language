1eae7a4f9679503e88cdf8358168089807940521
Joshua Goller <joshua.goller@uber.com>
2018-03-18 00:25:05 -0700

    5.1.c

--- Makefile
+++ Makefile
@@ -5,15 +5,15 @@ FLAGS := -std=c11 -g -Weverything -Werror
 all: signed-unsigned-ints
 
 clean:
-	-rm out/*/*
+	-rm bin/*/*/*
 
 setup:
-	mkdir -p out/ch-{1,2,3,4,5,6,7,8}/{examples,exercises,faq}
+	mkdir -p bin/ch-{1,2,3,4,5,6,7,8}/{examples,exercises,faq}
 
 5.1-getch:
-	$(CC) $(FLAGS) -I include/ch-5/ src/ch-5/examples/$@.c -o out/ch-5/examples/$@
+	$(CC) $(FLAGS) -I include/ch-5/ src/ch-5/examples/$@.c -o bin/ch-5/examples/$@
 	./out/ch-5/examples/$@
 
 5.1 5.2:
-	$(CC) $(FLAGS) -I include/ch-5/ src/ch-5/exercises/$@.c -o out/ch-5/exercises/$@
-	./out/ch-5/exercises/$@
+	$(CC) $(FLAGS) -I include/ch-5/ src/ch-5/exercises/$@.c -o bin/ch-5/exercises/$@
+	./bin/ch-5/exercises/$@
---
+++ src/ch-5/exercises/5.1.c
@@ -0,0 +1,70 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "5.1-getch.h"
+
+#define BUFSIZE 100
+
+static int buf[BUFSIZE];  // buffer for ungetch
+static int bufp = 0;
+
+// getint: get the next inter from input into *pn
+int getint(int *pn) {
+  int c, sign;
+
+  while (isspace(c = getch())) {
+    // no op; skip whitespace
+  }
+
+  // If the first char after whitespace isn't a number,
+  // we aren't reading a number.
+  if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+    ungetch(c);
+    return 0;
+  }
+
+  // Determine sign and skip sign char; catch case for "-" being the only input
+  sign = (c == '-') ? -1 : 1;
+  if (c == '-') {
+    c = getch();
+    if (c == '\n') {
+      printf("Error: '-' is invalid.\n");
+      return -1;
+    }
+  }
+
+  // Get each number, keeping a running product
+  for (*pn = 0; isdigit(c); c = getch()) {
+    *pn = 10 * *pn + (c - '0');
+  }
+
+  *pn *= sign;
+
+  if (c != EOF) {
+    ungetch(c);
+  }
+
+  return c;
+}
+
+int getch() {
+  int gotten;
+  gotten = (bufp > 0) ? buf[--bufp] : getchar();
+  // printf("Got this: %c == %d\n", gotten, gotten);
+  return gotten;
+}
+
+void ungetch(int c) {
+  if (bufp >= BUFSIZE) {
+    printf("ungetch: too many characters\n");
+  } else {
+    buf[bufp++] = c;
+  }
+}
+
+int main() {
+  int result = 0;
+  printf("Enter a number representable as an int: ");
+  getint(&result);
+  printf("You entered: %d\n", result);
+  return 0;
+}

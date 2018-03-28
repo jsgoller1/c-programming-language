57b2dac7ecdc68493a148a40a212f23d148cf525
Joshua Goller <jsgoller1@gmail.com>
2018-03-28 01:33:11 -0700

    solves 5.10

--- ch-5/include/5.10.h
+++ ch-5/include/5.10.h
@@ -1,3 +1,3 @@
 int is_number(char* num_string, int* val);
 void push(int val);
-int pop();
+int pop(void);
--- ch-5/src/5.10.c
+++ ch-5/src/5.10.c
@@ -1,4 +1,7 @@
+#include "5.10.h"
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
 Write the program expr(), which evaluates a reverse polish expression from the
@@ -6,43 +9,84 @@ command line, where each operator and operand is a separate argument. For
 example, "expr 2 3 4 + *" evaluates "2 x (3 + 4)".
 */
 
-#define MAX_VALS 100
+#define MAX_SIZE 100
 
-static vals_index = 0;
-static int vals[MAX_VALS];
+static int top = -1;
+static int stack[MAX_SIZE];
 
-void push(int val) {}
+void push(int val) {
+  if (top >= MAX_SIZE - 1) {
+    printf("Error: stack full, cannot push more values.\n");
+  }
+  stack[++top] = val;
+}
 
-int pop() {}
+int pop() {
+  int ret;
+  if (top == -1) {
+    printf("Error: stack empty.\n");
+    return -1;
+  }
+  ret = stack[top--];
+  return ret;
+}
 
-int is_number(char* num_string, int* val) { return 1; }
+int is_number(char* num_string, int* val) {
+  for (int i = 0; num_string[i] != '\0'; i++) {
+    if (!(isdigit(num_string[i]))) {
+      return 0;
+    }
+  }
+  *val = atoi(num_string);
+  return 1;
+}
 
 int main(int argc, char* argv[]) {
+  int op1, op2;
   for (int i = 1; i < argc; i++) {
-    // TODO: do input sanitization, quit on invalid input
-    char* current_arg = argv[argc];
+    char* current_arg = argv[i];
     int arg_val;
 
     if (is_number(current_arg, &arg_val)) {
       push(arg_val);
-    } else
-      switch (*(argv[argc])) {
+    } else {
+      switch (*(current_arg)) {
         case '+':
-          // handle
+          op1 = pop();
+          op2 = pop();
+          push(op1 + op2);
           break;
         case '-':
-          // handle
+          op2 = pop();  // reverse order because - doesn't commute
+          op1 = pop();
+          push(op1 - op2);
           break;
         case '/':
-          // handle
+          op2 = pop();  // reverse order because / doesn't commute
+          op1 = pop();
+          if (op2 == 0) {
+            printf("Error: division by zero.\n");
+            return -1;
+          }
+          push(op1 / op2);
           break;
         case '*':
-          // handle
+          op1 = pop();
+          op2 = pop();
+          push(op1 * op2);
           break;
         default:
-          printf("Error: invalid input: %s\n", *(argv[argc]));
+          printf("Error: invalid input: %s\n", argv[argc]);
+          printf("Usage: expr <rpn expression using +,*,-,/.\n");
           return -1;
       }
+    }
+  }
+  if (top != 0) {
+    printf("Error: invalid expression.\n");
+    printf("Usage: expr <rpn expression using +,*,-,/.\n");
+  } else {
+    printf("Result: %d\n", pop());
   }
   return 0;
 }

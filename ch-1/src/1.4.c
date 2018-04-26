554e80bb0ae506737a4c4b20cd3786d518677b49
Joshua Goller <joshua.goller@uber.com>
2018-04-25 20:43:44 -0700

    1.4 compiles without error

--- ch-1/src/1.4.c
+++ ch-1/src/1.4.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-/*  Ex 1-4: Write a program to convert celcius
-    temps into Fahrenheit. Since C = 5/9 * (F-32),
-    F = (C * 9/5) + 32
+/*
+Ex 1.4: Write a program to convert celcius
+temps into Fahrenheit. Since C = 5/9 * (F-32),
+F = (C * 9/5) + 32
 */
 
 int main() {
-  float fahr, celsius;
-  int lower, upper, step;
+  double fahr, celsius, lower, upper, step;
 
-  lower = 0;    // lower limit of temperature table
-  upper = 300;  // upper limit
-  step = 20;    // size increment
+  lower = 0.0;    // lower limit of temperature table
+  upper = 300.0;  // upper limit
+  step = 20.0;    // size increment
 
   celsius = lower;
   printf("C\tF\n");

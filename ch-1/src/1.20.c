c15f17e6f818cbde485b789133053ceca8f1fb9e
Joshua Goller <jsgoller1@gmail.com>
2018-04-27 00:53:18 -0700

    working but nasty implementation of detab

--- ch-1/src/1.20.c
+++ ch-1/src/1.20.c
@@ -1,46 +1,66 @@
 #include "1.20.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "common.h"
 
-// Write a function that replaces tabs with the correct number of whitespaces
-// (or any other character).
+/*
+Ex 1.20: Write a program detab() that replaces tabs in the input with the
+proper number of blanks to space to the next tab stop. Assume a fixed set of
+tab stops, say every n columns. Should n be a variable or a symbolic parameter?
+---
+I am not sure what "symbolic parameter" means here, but a "tab stop" means
+the following: suppose that you declare every 10 columns to be a tab stop.
+Then entering 5 characters and pressing tab causes columns 1 through 5 to be
+populated with keystroke characters, and columns 6 through 10 to be populated
+with whitespaces. Entering another 9 characters and pressing tab populates
+columns 11 through 19 with characters and column 20 with a whitespace.
+*/
 
-// ALWAYS USE CURLY BRACES, YOU IDIOT
+/*
+typedef struct str {
+  char* data;
+  int len;
+} string;
+*/
 
 int main() {
-  int len, i;          // current line length, and string index
-  char line[MAXLINE];  // current input line
+  // int len, i;          // current line length, and string index
+  // char line[MAXLINE];  // current input line
 
-  while ((len = getline(line, MAXLINE)) > 0) {
-    detab(line, len, 4);
-    printf("%s\n", line);
-  }
+  /*
+    while ((len = mygetline(line, MAXLINE)) > 0) {
+      detab(line, len, 4);
+      printf("%s\n", line);
+    }
+  */
+
+  detab("this\thas\ttwo", 12, 5);
   return 0;
 }
 
-// detab(): given a string of len, return it with all tabs replaced by
-// whitespaces-long sets of whitespace (using "." instead of whitespace for
-// clarity.)
-void detab(char string[], int len, int whitespaces) {
-  char copy[MAXLINE];
-  int i, j, k;
-  k = 0;
-
-  for (i = 0; i < len; i++) {
-    if (string[i] == '\t') {
-      for (j = 0; j < whitespaces; j++) {
-        copy[k] = '.';
-        k++;
-      }
+// detab():
+void detab(char line[], const int len, const int tab_stop) {
+  int i, j;
+  char new[MAXLINE];
+  for (j = i = 0; i < len; i++) {
+    if (line[i] != '\t') {
+      new[j] = line[i];
+      j++;
     } else {
-      copy[k] = string[i];
-      k++;
+      while (j % tab_stop) {
+        // printf("Filling in tab...\n");
+        new[j] = '.';
+        j++;
+      }
     }
   }
-
-  for (i = 0; i < k; i++) {
-    string[i] = copy[i];
-  }
-
-  string[k] = '\0';
+  new[j] = '\0';
+  printf("%s\n", new);
 }
+
+/*
+// stringify(): create a null-terminated string of len characters in line
+string* stringify(char line[], int len) {
+  string* str = (string*)malloc(sizeof(string));
+}
+*/

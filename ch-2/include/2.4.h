30e342c641d64ce061937c09c5cce5350191226c
Joshua Goller <jsgoller1@gmail.com>
2018-04-29 22:27:56 -0700

    2.4

---
+++ ch-2/include/2.4.h
@@ -0,0 +1,4 @@
+char* squeeze(char* const s1, const int len1, const char* const s2,
+              const int len2);
+int charmatch(const char c, const char* const str, const int len);
+void test(char* const str1, const char* const str2);
--- ch-2/src/2.4.c
+++ ch-2/src/2.4.c
@@ -1,52 +1,68 @@
+#include "2.4.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// Write an alternative version of squeeze() that deletes
-// any character in s1 that is found in another string s2
-
-void squeeze(char s1[], char s2[]);
+/*
+Ex 2.4: Write an alternate version of squeeze(s1 ,s2) that deletes
+each character in s1 that matches any character in the string s2.
+*/
 
 int main() {
-  char string1[] = {"dog food tastes good!"};
-  char string2[] = {"dgft"};
-  printf("%s - removing '%s': ", string1, string2);
-  squeeze(string1, string2);
   // o oo ases oo!
-  printf("%s\n", string1);
+  test("dog food tastes good!", "dgft");
 
-  char string3[] = {"dog food tastes good!"};
-  char string4[] = {""};
-  printf("%s - removing '%s': ", string3, string4);
-  squeeze(string3, string4);
+  // dg fd tsts gd
+  test("dog food tastes good!", "oae");
+
   // dog food tastes good!
-  printf("%s\n", string3);
+  test("dog food tastes good!", "jqx");
 
-  char string5[] = {""};
-  char string6[] = {"dog food tastes good!"};
-  printf("%s - removing '%s': ", string5, string6);
-  squeeze(string5, string6);
-  // empty
-  printf("%s\n", string5);
+  // dog food tastes good!
+  test("dog food tastes good!", "");
+
+  // ""
+  test("", "dog food tastes good!");
+
   return 0;
 }
 
-// delete all c from s
-void squeeze(char s1[], char s2[]) {
-  int i = 0;  // keeps track of our current place in s1 that we are able to put
-              // the next letter at
-  int j;      // used for looping through s2
-  int k;      // keeps track of the current letter in s1 under consideration
-  int MATCH_FOUND;
-  for (k = 0; s1[k] != '\0'; k++) {
-    MATCH_FOUND = 0;
-    for (j = 0; s2[j] != '\0' && !MATCH_FOUND; j++) {
-      if (s1[k] == s2[j]) {
-        MATCH_FOUND = 1;
-      }
-    }
+// squeeze(): delete all chars from s1 that are present in s2; callee
+// is responsible for freeing the returned string.
+char* squeeze(char* const s1, const int len1, const char* const s2,
+              const int len2) {
+  int i, j;
+  // the squeezed string will never be larger than the original,
+  // so while possibly wasteful, using len1 is sufficient
+  char* squeezed = (char*)malloc((unsigned long)len1 + 1);
 
-    if (!MATCH_FOUND) {
-      s1[i++] = s1[k];
+  for (i = 0, j = 0; i < len1; i++) {
+    if (charmatch(s1[i], s2, len2) == -1) {
+      squeezed[j] = s1[i];
+      j++;
     }
   }
-  s1[i] = '\0';
+
+  squeezed[i] = '\0';
+  return squeezed;
+}
+
+// charmatch(): returns index of c in str1, or -1 if not found.
+int charmatch(const char c, const char* const str, const int len) {
+  int i;
+  for (i = 0; i < len; i++) {
+    if (str[i] == c) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void test(char* const str1, const char* const str2) {
+  printf("%s - removing '%s': ", str1, str2);
+  int len1 = (int)strlen(str1);
+  int len2 = (int)strlen(str2);
+  char* squeezed = squeeze(str1, len1, str2, len2);
+  printf("%s\n", squeezed);
+  free(squeezed);
 }

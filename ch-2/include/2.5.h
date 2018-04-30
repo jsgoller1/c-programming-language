802b5a04a7b6e4af15b3a9d552cd0344203ef917
Joshua Goller <jsgoller1@gmail.com>
2018-04-29 22:58:57 -0700

    2.5

---
+++ ch-2/include/2.5.h
@@ -0,0 +1,3 @@
+int any(const char* const s1, const int len1, const char* const s2,
+        const int len2);
+void test(const char* const s1, const char* const s2);
--- ch-2/src/2.5.c
+++ ch-2/src/2.5.c
@@ -1,4 +1,6 @@
+#include "2.5.h"
 #include <stdio.h>
+#include <string.h>
 
 /*
 Write the function any(s1 ,s2), which returns the first location
@@ -7,36 +9,31 @@ contains no characters from s2. (The standard library function strpbrk does
 the same job but returns a pointer to the location.)
 */
 
-int any(char s1[], char s2[]);
-
 int main() {
-  char string1[] = {"Joshua"};
-  char string2[] = {"Slavoj"};
   // 1: "o" is found at 4 in string 2 and 1 in string 1 (J != j)
-  printf("%d\n", any(string1, string2));
+  test("Joshua", "Slavoj");
 
-  char string3[] = {"Joshua"};
-  char string4[] = {""};
+  // 1: "i" is in dennis, and is at 1 in Nick
+  test("Nick", "Dennis");
+
   // -1
-  printf("%d\n", any(string3, string4));
+  test("King", "Queer");
 
-  char string5[] = {""};
-  char string6[] = {"Slavoj"};
   // -1
-  printf("%d\n", any(string5, string6));
+  test("Joshua", "");
 
-  char string7[] = {"Nick"};
-  char string8[] = {"Dennis"};
-  // 1
-  printf("%d\n", any(string7, string8));
+  // -1
+  test("", "Joshua");
 
   return 0;
 }
 
-int any(char s1[], char s2[]) {
+// any(): find first occurence in s1 of a char in both s1 and s2
+int any(const char* const s1, const int len1, const char* const s2,
+        const int len2) {
   int i, j;
-  for (i = 0; s1[i] != '\0'; i++) {
-    for (j = 0; s2[j] != '\0'; j++) {
+  for (i = 0; i < len1; i++) {
+    for (j = 0; j < len2; j++) {
       if (s1[i] == s2[j]) {
         return i;
       }
@@ -44,4 +41,14 @@ int any(char s1[], char s2[]) {
   }
 
   return -1;
+}
+void test(const char* const s1, const char* const s2) {
+  const int len1 = (int)strlen(s1);
+  const int len2 = (int)strlen(s2);
+  const int i = any(s1, len1, s2, len2);
+  if (i != -1) {
+    printf("%c in %s (at %d) and %s.\n", s1[i], s1, i, s2);
+  } else {
+    printf("%s and %s share no characters.\n", s1, s2);
+  }
 }

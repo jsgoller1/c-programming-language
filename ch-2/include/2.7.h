3c5092bad2cccad039bb95e9e0ab42c5d0c21f83
Joshua Goller <joshua.goller@uber.com>
2018-04-30 22:36:57 -0700

    2.7

---
+++ ch-2/include/2.7.h
@@ -0,0 +1,3 @@
+int invert(const int x, const int position, const int count);
+int test(const int x, const int position, const int count,
+         const int hypothesis);
--- ch-2/src/2.7.c
+++ ch-2/src/2.7.c
@@ -1,70 +1,34 @@
+#include "2.7.h"
 #include <stdio.h>
+#include "common.h"
 
 /*
-Write a function setbits(x,p,n) that inverts the n bits starting at p:
+Write a function setbits(x,p,n) that inverts the n bits starting at p. Assuming
+p = 1 and n = 3
 
-Assuming p = 5 and n = 3
+       3 2 p
+x = 0b 1 0 1 0
+becomes
+x = 0b 0 1 0 0
+---
+Bit flipping can be done via xor - set the bits to flip and leave the other
+ones unset:
+0 1 0 1 src
+1 1 1 1 mask
+1 0 1 0 (flipped)
 
-               p 2 3
-x = 0b 1 1 0 0 1 1 0 0 1 1
-returns
-x = 0b 1 1 0 0 0 0 1 0 1 1
+0 1 0 1 src
+0 0 0 0 mask
+0 1 0 1 (presered)
+
+The solution to this problem is:
+1) Create bitmask with bits-to-be-flipped set and others not.
+2) xor with x.
 */
 
-unsigned invert(int x, int position, int count);
+int main() { return 0; }
 
-int main() {
-  // 0b10100110 = 0xA6
-  printf("%x\n", invert(0b10101010, 3, 2));
-  // 0b00111000 = 0x38
-  printf("%x\n", invert(0b00110100, 3, 2));
-  // 0x0F
-  printf("%x\n", invert(0, 3, 4));
-  // 0xF0
-  printf("%x\n", invert(0b11111111, 3, 4));
-  return 0;
-}
-
-unsigned invert(int x, int position, int count) {
-  if (count == 0) {
-    return x;
-  }
-  int mask, trim_mask, inversion_bits, new_x;
-  mask = trim_mask = 0;
-
-  // Set mask; see exercise 2.6 for explanation
-  mask = ~(~mask << position + 1);
-  if (!(position < count)) {
-    trim_mask = ~(~trim_mask << (position - count + 1));
-  }
-  mask = mask ^ trim_mask;
-
-  // Get required bits from x, inverted; to do this:
-  // inversion_bits = mask AND x
-  // inversion_bits = ~inversion_bits
-  // inversion_bits = mask & inversion_bits
-  //                       ||
-  //           mask: 0b00001100
-  //              x: 0b10101010
-  //                       ||
-  // inversion_bits: 0b00001000
-  //~inversion_bits: 0b11110111
-  //                       ||
-  //           mask: 0b00001100
-  //                       =
-  // inversion_bits: 0b00000100
-  inversion_bits = mask & x;
-  inversion_bits = ~inversion_bits;
-  inversion_bits = mask & inversion_bits;
-
-  // Finally, set them in x. We have:
-  //          ~mask: 0b11110011
-  // inversion_bits: 0b00000100
-  //              x: 0b10101010
-  //          new_x: 0b10100110
-  // So to set our new x, we want the
-  // current bits of x, except the inverted ones
-  new_x = (x & (~mask)) | inversion_bits;
-
-  return new_x;
+int invert(const int x, const int position, const int count) {
+  const int mask = create_mask(const int position, const int count);
+  return (x ^ mask);
 }

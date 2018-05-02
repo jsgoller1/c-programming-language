2d7e260cc7360fff38bcf81d1eb01a5c363e9d4e
Joshua Goller <joshua.goller@uber.com>
2018-05-02 00:37:28 -0700

    3.2

---
+++ ch-3/include/3.2.h
@@ -0,0 +1,3 @@
+char* escape(const char* const str, const int t_len);
+char* unescape(const char* const str, const int t_len);
+void test(const char* const str);
--- ch-3/src/3.2.c
+++ ch-3/src/3.2.c
@@ -1,110 +1,92 @@
+#include "3.2.h"
 #include <stdio.h>
-
-#define MAX_LEN 1000
+#include <stdlib.h>
+#include <string.h>
+#include "common.h"
 
 /*
-Write a function escape(s, t) that converts characters like
+Ex 3.2: Write a function escape(s, t) that converts characters like
 newline and tab into visible escape sequences like \n and \t as it copies the
 string t to s. Use a switch. Write a function for the other direction as well,
 converting escape sequences into the real characters.
 */
 
-void escape(char s[], char t[], int t_len);
-void unescape(char s[], char t[], int t_len);
-
 int main() {
-  char in[] = {"a\nb\nc\td\n\te\n"};
-  char out[MAX_LEN];
-  char final[MAX_LEN];
-  escape(in, out, MAX_LEN);
-  printf("%s\n%s\n\n", in, out);
-  unescape(out, final, MAX_LEN);
-  printf("%s\n%s\n", out, final);
-
-  printf("----------------------------------------\n");
-
-  char in1[] = {""};
-  char out1[MAX_LEN];
-  char final1[MAX_LEN];
-  escape(in1, out1, MAX_LEN);
-  printf("%s\n%s\n", in1, out1);
-  unescape(out1, final1, MAX_LEN);
-  printf("%s\n%s\n", out1, final1);
-
-  printf("----------------------------------------\n");
-
-  char in2[] = {"asdasdsadsada"};
-  char out2[MAX_LEN];
-  char final2[MAX_LEN];
-  escape(in2, out2, MAX_LEN);
-  printf("%s\n%s\n", in2, out2);
-  unescape(out2, final2, MAX_LEN);
-  printf("%s\n%s\n", out2, final2);
-
+  test("a\nb\nc\td\n\te\n");
+  test("");
   return 0;
 }
 
-void escape(char s[], char t[], int t_len) {
-  int i, j;
-  i = j = 0;
+void test(const char* const str) {
+  printf("Original: %s\n", str);
 
-  while (s[i] != '\0' && i < t_len) {
-    switch (s[i]) {
-      case '\n':
-        t[j] = '\\';  // Technically, we could increment i on the same line, but
-        j++;  // that makes it harder to guess what index will be used in the
-              // array.
-        t[j] = 'n';
-        j++;
-        break;
-      case '\t':
-        t[j] = '\\';
-        j++;
-        t[j] = 't';
-        j++;
-        break;
-      default:
-        t[j] = s[i];
-        j++;
-        break;
-    }
-    i++;
-  }
-  t[j] = '\0';
+  char* escaped = escape(str, (int)strlen(str));
+  printf("Escaped: %s\n", escaped);
+  free(escaped);
+
+  char* unescaped = unescape(str, (int)strlen(str));
+  printf("Unescaped: %s\n", unescaped);
+  free(unescaped);
+
+  printf("----------------------------------------\n");
 }
 
-void unescape(char s[], char t[], int t_len) {
+char* escape(const char* const str, const int len) {
   int i, j;
-  i = j = 0;
 
-  while (s[i] != '\0' && i < t_len) {
-    switch (s[i]) {
-      case '\\':
-        if (s[i + 1] == 'n') {
-          t[j] = '\n';
-          j++;
-          i += 2;
-        } else if (s[i + 1] == 't') {
-          t[j] = '\t';
-          j++;
-          i += 2;
-        } else {
-          t[j] = s[i];  // We could probably be clever here and nest the breaks
-                        // inside the else-if clauses, letting anything
-          i++;  // not matching \n or \t fall through and be treated as a normal
-                // character, but that would be less clear.
-          j++;
-          t[j] = s[i];
-          i++;
-          j++;
-        }
+  /*
+  if every char in the string is an escape char,
+  the returned string will be twice as long and
+  will require a null terminator.
+  */
+  char* escaped = (char*)malloc((unsigned long)(len * 2) + 1);
+
+  for (i = 0, j = 0; i < len; i++) {
+    switch (str[i]) {
+      case '\n':
+        escaped[j++] = '\\';
+        escaped[j++] = 'n';
+        break;
+      case '\t':
+        escaped[j++] = '\\';
+        escaped[j++] = 't';
         break;
       default:
-        t[j] = s[i];
-        i++;
-        j++;
+        escaped[j++] = str[i];
         break;
     }
   }
-  t[j] = '\0';
+
+  escaped[j] = '\0';
+  return escaped;
+}
+
+char* unescape(const char* const str, const int len) {
+  int i, j;
+
+  /*
+  if every char in the string is an escaped char,
+  the returned string will be half as long and
+  will require a null terminator.
+  */
+  char* unescaped = (char*)malloc((unsigned long)(len * 2) + 1);
+
+  for (i = 0, j = 0; i < len; i++) {
+    unescaped[j++] = str[i];
+    if (str[i] == '\\') {
+      switch (str[++i]) {
+        case 'n':
+          unescaped[j++] = '\n';
+          break;
+        case 't':
+          unescaped[j++] = '\n';
+          break;
+        default:
+          unescaped[j++] = str[i];
+          break;
+      }
+    }
+  }
+  unescaped[j] = '\0';
+  return unescaped;
 }

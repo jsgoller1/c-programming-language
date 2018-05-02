136b4d987edec84b38034d0b91de5dd96f3de9c7
Joshua Goller <joshua.goller@uber.com>
2018-05-02 12:24:28 -0700

    3.3 - const correctness and some cleanup

---
+++ ch-3/include/3.3.h
@@ -0,0 +1,7 @@
+char* expand(const char* const str, const int len);
+int is_valid_range(const char a, const char b);
+int get_char_type(const char a);
+int is_upper(const char a);
+int is_lower(const char a);
+int is_int(const char a);
+void test(const char* const str);
--- ch-3/src/3.3.c
+++ ch-3/src/3.3.c
@@ -1,169 +1,134 @@
+#include "3.3.h"
 #include <stdio.h>
-#define MAX_LEN 1000
+#include <stdlib.h>
+#include <string.h>
+#include "common.h"
 
 /*
-Exercise 3-3. Write a function expand(s1 ,s2) that expands shorthand
+Exercise 3-3. Write a function expand(s1, s2) that expands shorthand
 notations like a-z in the string s1 into the equivalent complete list abc...xyz
 in s2. Allow for letters of either case and digits, and be prepared to handle
 cases like a-b-c and a-zO-9 and -a-z. Arrange that a leading or trailing - is
 taken literally.
-
+----
 For this, I'm assuming that:
-a-1 -> a-1, a-A -> a-A, etc. (no expansion for different chars)
+(no expansion for different chars)
+a-1 -> a-1,
+a-A -> a-A, etc.
 d-a -> dcba (reverse expansion)
 a-c-e -> abc-e -> abcde (chained expansion)
 */
 
-void expand(char s1[], int s1_len, char s2[]);
-int is_valid_range(char a, char b);
-int get_type(char a);
-int is_upper(char a);
-int is_lower(char a);
-int is_int(char a);
-
 int main() {
-  // Note: did some copying and pasting
-  // to organize test case; apologies
-  // for numbering being disorganized
-  // (as long as the test vars are
-  // grouped correctly, I don't really
-  // care here)
-
   // basic expansion, lower case
   // abcd
-  char t0[] = {"a-d"};
-  char a0[MAX_LEN];
-  expand(t0, 3, a0);
-  printf("test 0: %s == %s\n", t0, a0);
+  test("a-d");
 
   // basic expansion, upper case
   // ABCD
-  char t1[] = {"A-D"};
-  char a1[MAX_LEN];
-  expand(t1, 3, a1);
-  printf("test 1: %s == %s\n", t1, a1);
+  test("A-D");
 
   // basic expansion, ints
   // 01234
-  char t2[] = {"0-4"};
-  char a2[MAX_LEN];
-  expand(t2, 3, a2);
-  printf("test 2: %s == %s\n", t2, a2);
+  test("0-4");
 
   // Average input
   // abcd01234ABCD
-  char t3[] = {"a-d0-4A-D"};
-  char a3[MAX_LEN];
-  expand(t3, 9, a3);
-  printf("test 3: %s == %s\n", t3, a3);
+  test("a-d0-4A-D");
 
   // Reverse input
   // dcba
-  char t4[] = {"d-a"};
-  char a4[MAX_LEN];
-  expand(t4, 3, a4);
-  printf("test 4: %s == %s\n", t4, a4);
+  test("d-a");
 
   // null input
   // ""
-  char t5[] = {""};
-  char a5[MAX_LEN];
-  expand(t5, 0, a5);
-  printf("test 5: %s == %s\n", t5, a5);
+  test("");
 
   // edge cases, all of which should
   // print their input
-  char t6[] = {"-"};
-  char a6[MAX_LEN];
-  expand(t6, 2, a6);
-  printf("test 6: %s == %s\n", t6, a6);
-
-  char t7[] = {"-a-"};
-  char a7[MAX_LEN];
-  expand(t7, 3, a7);
-  printf("test 7: %s == %s\n", t7, a7);
-
-  char t8[] = {"a--a"};
-  char a8[MAX_LEN];
-  expand(t8, 4, a8);
-  printf("test 8: %s == %s\n", t8, a8);
-
-  char t9[] = {"abcd"};
-  char a9[MAX_LEN];
-  expand(t9, 4, a9);
-  printf("test 9: %s == %s\n", t9, a9);
-
-  char t10[] = {"1-a"};
-  char a10[MAX_LEN];
-  expand(t10, 3, a10);
-  printf("test 10: %s == %s\n", t10, a10);
-
+  test("-");
+  test("-a-");
+  test("a--a");
+  test("abcd");
+  test("1-a");
   return 0;
 }
 
-void expand(char s1[], int s1_len, char s2[]) {
-  int i;  // s1 index
-  int j;  // s2 index
-  int k;  // temp var for expansion
-  j = 0;
+void test(const char* const str) {
+  char* expanded = expand(str, (int)strlen(str));
+  printf("original: %s\n", str);
+  printf("expanded: %s\n\n", expanded);
+  free(expanded);
+}
+
+char* expand(const char* const str, const int len) {
+  int i;   // s1 index
+  int j;   // s2 index
+  char k;  // temp var for expansion
+
+  // We will assume that expand() will never
+  // expand to more than 1000 characters,
+  // which is not at all a reasonable assumption.
+  char* expanded = (char*)malloc((unsigned long)1000);
 
   // go through the string until we find a '-'
-  for (i = 0; i < s1_len; i++) {
+  for (i = 0, j = 0; i < len; i++) {
     // check if the chars left and right of it are within bounds
     // printf("i: %d\n", i);
-    if ((s1[i] == '-') && ((i - 1 >= 0) && (i + 1 < s1_len))) {
+    if ((str[i] == '-') && ((i - 1 >= 0) && (i + 1 < len))) {
       // if so, determine if specificed range is of valid "types" (upper, lower,
       // or int, both bounds are the same "type")
-      if (is_valid_range(s1[i - 1], s1[i + 1])) {
+      if (is_valid_range(str[i - 1], str[i + 1])) {
         // given that they're the same type, determine if they're ascending or
         // descending
-        if (s1[i - 1] < s1[i + 1]) {
+        if (str[i - 1] < str[i + 1]) {
           // for ascending, expand by incrementing; remember
           // the left bound is already in s2 from the previous
           // iteration, so skip it.
-          for (k = (s1[i - 1] + 1); k <= s1[i + 1]; k++) {
-            s2[j] = k;
+          for (k = (str[i - 1] + 1); k <= str[i + 1]; k++) {
+            expanded[j] = k;
             j++;
           }
           i++;  // in a-c, don't evaluate c after evaluating -;
-        } else if (s1[i - 1] > s1[i + 1]) {
+        } else if (str[i - 1] > str[i + 1]) {
           // for descending, expand by looping down; remember
           // the left bound is already the first correct char
-          for (k = (s1[i - 1] - 1); k >= s1[i + 1]; k--) {
-            s2[j] = k;
+          for (k = (str[i - 1] - 1); k >= str[i + 1]; k--) {
+            expanded[j] = k;
             j++;
           }
           i++;  // in c-a, don't evaluate a after evaluating -;
 
         } else  // they're equal, just copy the character;
         {
-          s2[j] = s1[i];
+          expanded[j] = str[i];
           j++;
         }
       } else  // invalid range bounds, copy the '-' mark
       {
-        s2[j] = s1[i];
+        expanded[j] = str[i];
         j++;
       }
     } else  // either left or right is outside of s1 or the s[i] != '-'; copy
             // s[i]
     {
-      s2[j] = s1[i];
+      expanded[j] = str[i];
       j++;
     }
   }
-  s2[j] = '\0';
+  expanded[j] = '\0';
+  return expanded;
 }
 
 int is_valid_range(char a, char b) {
   int type_a, type_b;
-  type_a = get_type(a);
-  type_b = get_type(b);
+  type_a = get_char_type(a);
+  type_b = get_char_type(b);
 
   return ((type_a == type_b) && (type_a != -1));
 }
 
-int get_type(char a) {
+int get_char_type(const char a) {
   if (is_upper(a)) {
     return 0;
   } else if (is_lower(a)) {
@@ -175,8 +140,8 @@ int get_type(char a) {
   }
 }
 
-int is_upper(char a) { return (a >= 'A') && (a <= 'Z'); }
+int is_upper(const char a) { return (a >= 'A') && (a <= 'Z'); }
 
-int is_lower(char a) { return (a >= 'a') && (a <= 'z'); }
+int is_lower(const char a) { return (a >= 'a') && (a <= 'z'); }
 
-int is_int(char a) { return (a >= '0') && (a <= '9'); }
+int is_int(const char a) { return (a >= '0') && (a <= '9'); }

5ec2eb15e9da44e46786178e3dbd5cfd6aad47a9
Joshua Goller <jsgoller1@gmail.com>
2018-05-06 22:40:35 -0700

    conventionalize 1.9

--- ch-1/src/1.9.c
+++ ch-1/src/1.9.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-/*
-Ex 1.9: Write a program to copy its input to its output,
-replacing each string of one or more blanks with a single blank.
-*/
+#ifndef VERBOSE_TEST
+#pragma clang diagnostic ignored "-Wunused-function"
+#endif
 
-int main() {
+/*
+ * Ex 1.9: Write a program to copy its input to its output,
+ * replacing each string of one or more blanks with a single blank.
+ */
+
+static int replace_blanks() {
   int c = 0, space = 0;
 
   printf("Begin typing, terminate via ctrl-D on an empty line.\n");
@@ -21,4 +25,12 @@ int main() {
     }
   }
   return 0;
+}
+
+int main() {
+#ifdef VERBOSE_TEST
+  replace_blanks();
+#else
+  printf("1.9: No unit tests.\n");
+#endif
 }

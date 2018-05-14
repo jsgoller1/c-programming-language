0eda7b99f59f0c6f6dfd71b68bbf0b9c7bc7c172
Joshua Goller <jsgoller1@gmail.com>
2018-05-14 00:04:50 -0700

    clean up tests so we dont have to handle print flags everywhere

--- tests/include/tests.h
+++ tests/include/tests.h
@@ -1,18 +1,30 @@
 #include <stdbool.h>
 
-void assert_int_eq(const int a, const int b);
-void assert_int_neq(const int a, const int b);
+void assert_int_eq(const int actual, const int expected,
+                   const char* const func_name, const char* const message);
+void assert_int_neq(const int actual, const int expected,
+                    const char* const func_name, const char* const message);
 
-void assert_double_eq(const double a, const double b, const double error);
-void assert_double_neq(const double a, const double b, const double error);
+void assert_double_eq(const double actual, const double expected,
+                      const double error, const char* const func_name,
+                      const char* const message);
+void assert_double_neq(const double actual, const double expected,
+                       const double error, const char* const func_name,
+                       const char* const message);
 
-void assert_string_eq(const char* const a, const char* const b);
-void assert_string_neq(const char* const a, const char* const b);
+void assert_string_eq(const char* const actual, const char* const expected,
+                      const char* const func_name, const char* const message);
+void assert_string_neq(const char* const actual, const char* const expected,
+                       const char* const func_name, const char* const message);
 
-void assert_true(const bool expr);
-void assert_false(const bool expr);
+void assert_true(const bool expr, const char* const func_name,
+                 const char* const message);
+void assert_false(const bool expr, const char* const func_name,
+                  const char* const message);
 
-void assert_mem_eq(const void* const obj_a, const void* const obj_b,
-                   const int size);
-void assert_mem_neq(const void* const obj_a, const void* const obj_b,
-                    const int size);
+void assert_mem_eq(const void* const actual, const void* const expected,
+                   const int size, const char* const func_name,
+                   const char* const message);
+void assert_mem_neq(const void* const actual, const void* const expected,
+                    const int size, const char* const func_name,
+                    const char* const message);
--- tests/src/tests.c
+++ tests/src/tests.c
@@ -12,39 +12,164 @@
  */
 
 // assert_ints_*(): compare integers
-void assert_int_eq(const int a, const int b) { assert(a == b); }
-void assert_int_neq(const int a, const int b) { assert(a != b); }
+void assert_int_eq(const int actual, const int expected,
+                   const char* const func_name, const char* const message) {
+#ifdef TEST_MESSAGES
+  printf("%s(): %s\n", func_name, message);
+#else
+  (void)func_name;
+  (void)message;
+#endif
+#ifdef DEBUG
+  printf("Expected: %d\n", expected);
+  printf("Actual: %d\n", actual);
+#endif
+  assert(actual == expected);
+}
+
+void assert_int_neq(const int actual, const int expected,
+                    const char* const func_name, const char* const message) {
+#ifdef TEST_MESSAGES
+  printf("%s(): %s\n", func_name, message);
+#else
+  (void)func_name;
+  (void)message;
+#endif
+#ifdef DEBUG
+  printf("Expected: %d\n", expected);
+  printf("Actual: %d\n", actual);
+#endif
+  assert(actual != expected);
+}
 
 // assert_double_*(): compare equality of doubles to arbitrary precision
-void assert_double_eq(const double a, const double b, const double error) {
-  const double difference = fabs(a - b);
-  assert(difference < error);
+void assert_double_eq(const double actual, const double expected,
+                      const double error, const char* const func_name,
+                      const char* const message) {
+  const double difference = fabs(actual - expected);
+#ifdef TEST_MESSAGES
+  printf("%s(): %s\n", func_name, message);
+#else
+  (void)func_name;
+  (void)message;
+#endif
+#ifdef DEBUG
+  printf("Expected: %f\n", expected);
+  printf("Actual: %f\n", actual);
+  printf("Difference: %f/n", difference)
+#endif
+      assert(difference < error);
 }
-void assert_double_neq(const double a, const double b, const double error) {
-  const double difference = fabs(a - b);
-  assert(difference > error);
+void assert_double_neq(const double actual, const double expected,
+                       const double error, const char* const func_name,
+                       const char* const message) {
+  const double difference = fabs(actual - expected);
+#ifdef TEST_MESSAGES
+  printf("%s(): %s\n", func_name, message);
+#else
+  (void)func_name;
+  (void)message;
+#endif
+#ifdef DEBUG
+  printf("Expected: %f\n", expected);
+  printf("Actual: %f\n", actual);
+  printf("Difference: %f/n", difference)
+#endif
+      assert(difference > error);
 }
 
 // assert_strings_*(): compare strings via strcmp(); assumes strings are null
 // terminated
-void assert_string_eq(const char* const a, const char* const b) {
-  assert(strcmp(a, b) == 0);
+void assert_string_eq(const char* const actual, const char* const expected,
+                      const char* const func_name, const char* const message) {
+#ifdef TEST_MESSAGES
+  printf("%s(): %s\n", func_name, message);
+#else
+  (void)func_name;
+  (void)message;
+#endif
+#ifdef DEBUG
+  printf("Expected: %s\n", expected);
+  printf("Actual: %s\n", actual);
+#endif
+  assert(strcmp(actual, expected) == 0);
 }
 
-void assert_string_neq(const char* const a, const char* const b) {
-  assert(strcmp(a, b) != 0);
+void assert_string_neq(const char* const actual, const char* const expected,
+                       const char* const func_name, const char* const message) {
+#ifdef TEST_MESSAGES
+  printf("%s(): %s\n", func_name, message);
+#else
+  (void)func_name;
+  (void)message;
+#endif
+#ifdef DEBUG
+  printf("Expected: %s\n", expected);
+  printf("Actual: %s\n", actual);
+#endif
+  assert(strcmp(actual, expected) != 0);
 }
 
 // assert_<bool>(): assert truthiness / falsity of expression
-void assert_true(const bool expr) { assert(expr); }
-void assert_false(const bool expr) { assert(!(expr)); }
+void assert_true(const bool expr, const char* const func_name,
+                 const char* const message) {
+#ifdef TEST_MESSAGES
+  printf("%s(): %s\n", func_name, message);
+#else
+  (void)func_name;
+  (void)message;
+#endif
+#ifdef DEBUG
+  printf("Expected: %d\n", expected);
+  printf("Actual: %d\n", 1);
+#endif
+  assert(expr);
+}
+void assert_false(const bool expr, const char* const func_name,
+                  const char* const message) {
+#ifdef TEST_MESSAGES
+  printf("%s(): %s\n", func_name, message);
+#else
+  (void)func_name;
+  (void)message;
+#endif
+#ifdef DEBUG
+  printf("Expected: %d\n", expected);
+  printf("Actual: %d\n", 0);
+#endif
+  assert(!(expr));
+}
 
 // assert_mem_*(): Compare regions of memory via memcmp()
-void assert_mem_eq(const void* const obj_a, const void* const obj_b,
-                   const int size) {
-  assert(memcmp(obj_a, obj_b, (unsigned long)size) == 0);
+void assert_mem_eq(const void* const actual, const void* const expected,
+                   const int size, const char* const func_name,
+                   const char* const message) {
+#ifdef TEST_MESSAGES
+  printf("%s(): %s\n", func_name, message);
+#else
+  (void)func_name;
+  (void)message;
+#endif
+  const int result = memcmp(actual, expected, (unsigned long)size);
+#ifdef DEBUG
+  printf("Expected: 0\n", expected);
+  printf("Actual: %d\n", result);
+#endif
+  assert(result == 0);
 }
-void assert_mem_neq(const void* const obj_a, const void* const obj_b,
-                    const int size) {
-  assert(memcmp(obj_a, obj_b, (unsigned long)size) != 0);
+void assert_mem_neq(const void* const actual, const void* const expected,
+                    const int size, const char* const func_name,
+                    const char* const message) {
+#ifdef TEST_MESSAGES
+  printf("%s(): %s\n", func_name, message);
+#else
+  (void)func_name;
+  (void)message;
+#endif
+  const int result = memcmp(actual, expected, (unsigned long)size);
+#ifdef DEBUG
+  printf("Expected: nonzero\n");
+  printf("Actual: %d\n", result);
+#endif
+  assert(result != 0);
 }

409ffc72ff04a92d6bbb4ef92c029c5ae1b6fd76
Joshua Goller <jsgoller1@gmail.com>
2018-05-15 00:38:21 -0700

    1.22

--- ch-1/src/1.22.c
+++ ch-1/src/1.22.c
@@ -3,22 +3,29 @@
 #include "common.h"
 
 /*
-Ex 1.22: Write a program to "fold" long input lines into two or more shorter
-lines after the last non-blank character that occurs before the n-th column of
-input. Make sure your program does something intelligent with very long lines,
-and if there are no blanks or tabs before the specified column.
-----
-I'm assuming this question is asking me to replicate the $fold program.
-*/
+ * Ex 1.22: Write a program to "fold" long input lines into two or more shorter
+ * lines after the last non-blank character that occurs before the n-th column
+ * of input. Make sure your program does something intelligent with very long
+ * lines, and if there are no blanks or tabs before the specified column.
+ * ----
+ * I'm assuming this question is asking me to replicate the $fold program.
+ */
 
+#ifdef DEBUG
 #define FOLD_COLUMN 5
+#endif
 
 int main() {
+#ifdef DEBUG
   char line[MAXLINE];
   int len;
+  printf("Begin entering text.\n");
   while ((len = mygetline(line, MAXLINE))) {
     fold(line, MAXLINE, FOLD_COLUMN);
   }
+#else
+  printf("1.22: No unit tests.\n");
+#endif
   return 0;
 }
 

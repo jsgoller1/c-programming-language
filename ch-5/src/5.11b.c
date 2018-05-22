171ae6f7d7ad69cebab55c323f6a74efce7e4005
Joshua Goller <jsgoller1@gmail.com>
2018-05-21 23:42:10 -0700

    5.11, but unfinished / untested

---
+++ ch-5/src/5.11b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "tests.h"
+
+/*
+ * Exercise 5-11. Modify the programs entab and detab (written as exercises in
+ * Chapter 1) to accept a list of tab stops as arguments. Use the default tab
+ * settings if there are no arguments,
+ */
+
+static void entab_test() {}
+static void detab_test() {}
+
+int main(int argc, char** argv) {}

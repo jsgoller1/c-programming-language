4d63e84999a6978b24dc53392e42b574479d1698
Joshua Goller <joshua.goller@uber.com>
2018-05-22 20:07:40 -0700

    fn pointers with sort

--- ch-5/include/5.14-17.h
+++ ch-5/include/5.14-17.h
@@ -1,3 +1,4 @@
 // From this exercise
 void swap_strs(char** v, const int i, const int j);
-void myqsort(char** strings, int left, int right);
+void myqsort(char** strings, int left, int right, int (*comp)(void*, void*));
+int numcmp(const char* const s1, const char* const s2);
--- ch-5/src/5.14-17.c
+++ ch-5/src/5.14-17.c
@@ -8,8 +8,10 @@
 int main() {
   char *lines[MAXLEN];
   const int nlines = readlines(lines, MAXLEN);
+  int numeric = 0;
 
-  myqsort(lines, 0, nlines - 1);
+  myqsort(lines, 0, nlines - 1,
+          (int (*)(void *, void *))(numeric ? numcmp : strcmp));
   writelines(lines, nlines);
   freelines(lines, nlines);
 
@@ -25,21 +27,39 @@ void swap_strs(char **v, const int i, const int j) {
   v[j] = temp;
 }
 
-// qsort(): sort v[left]...v[right] into increasing order
-void myqsort(char **strings, int left, int right) {
+// myqsort(): sort v[left]...v[right] into increasing order
+void myqsort(char **strings, int left, int right, int (*comp)(void *, void *)) {
   int i, last;
 
+  // Quit if empty array
   if (left >= right) {
     return;
   }
+
   swap_strs(strings, left, (left + right) / 2);
   last = left;
   for (i = left + 1; i <= right; i++) {
-    if (strcmp(strings[i], strings[left]) < 0) {
+    if ((*comp)(strings[i], strings[left]) < 0) {
       swap_strs(strings, ++last, i);
     }
   }
   swap_strs(strings, left, last);
-  myqsort(strings, left, last - 1);
-  myqsort(strings, last + 1, right);
+  myqsort(strings, left, last - 1, comp);
+  myqsort(strings, last + 1, right, comp);
+}
+
+// numcmp(): compare s1 and s2 numerically
+int numcmp(const char *const s1, const char *const s2) {
+  double v1, v2;
+
+  v1 = atof(s1);
+  v2 = atof(s2);
+
+  if (v1 < v2) {
+    return -1;
+  } else if (v1 > v2) {
+    return 1;
+  } else {
+    return 0;
+  }
 }

ab3ea4e93a5e50f3f53a677b43614e7d3d649b75
Joshua Goller <jsgoller1@gmail.com>
2018-06-06 22:23:07 -0700

    free_j() worked out finally

--- malloc_j/src/free_j.c
+++ malloc_j/src/free_j.c
@@ -1,6 +1,15 @@
+#include <stdbool.h>
 #include "malloc_j.h"
 
-// free_j(): put block ap in free list
+static void merge_chunks(header *first, header *second,
+                         bool preserve_first_next) {
+  first->size += second->size + sizeof(header);
+  if (!(preserve_first_next)) {
+    first->next = second->next;
+  }
+}
+
+// free_j(): insert a chunk into the free list
 int free_j(void *chunk) {
   header *ll_node, *p;
 
@@ -12,42 +21,49 @@ int free_j(void *chunk) {
   }
 
   /* find insertion point. There are three possibilities:
-    1. chunk's address is before list head; ll_node should be added and become
-    free_list.
-    2. chunk's address is after list tail; ll_node should become the tail.
-    3. chunk's addres is between list head and tail; find the correct place and
+    1. chunk's address is before list head; ll_node is prepended, and ll_node
+    becomes become free_list.
+    2. chunk's address is after list tail; ll_node should be appended to the
+    list.
+    3. chunk's address is between list head and tail; find the correct place and
     insert it.
   */
 
-  // handle case 1
-  if (ll_node < free_list) {
-    ll_node.next = free_list;
-    free_list = ll_node;
-    return;
-  }
-
-  // handle cases 2 and 3; we will either find the right place
-  // or reach the end of the list.
-  for (p = free_list; p < p->next; p = p->next) {
-    if (p < ll_node && ll_node < p->next) {
-      break;
+  // handle case 1, merging if necessary
+  if (chunk < free_list) {
+    if (chunk + chunk->size == free_list) {
+      merge_chunks(chunk, free_list, false);
+    } else {
+      chunk->next = free_list;
     }
+    free_list = chunk;
   }
 
-  // if contiguous as [ ll_node | p ], join as one block preserving ll_node
+  // handle cases 2 and 3
+  p = free_list;
+  while ((p < ll_node) && (p < p->next)) {
+    if (p == ll_node) {
+      printf("free_j() | warning: double free.\n")
+    }
+    if (ll_node < p->next) {
+      // ll_node falls between p and p->next
+      break;
+    };
+    p = p->next;
+  }
+
+  // contiguous as [ ll_node | p->next ], join blocks and keep ll_node
   if (ll_node + ll_node->size == p->next) {
-    ll_node->size += p->next->size + sizeof(header);
-    ll_node->next = p->next->next;
+    merge_chunks(ll_node, p->next, false);
     p->next = ll_node;
   }
 
-  // if contiguous as [ p | ll_node ], join as one block preserving p
+  // contiguous as [ p | ll_node ], join blocks and keep p
   else if (p + p->size == ll_node) {
-    p->size += ll_node->size + sizeof(header);
-    p->next = ll_node->next;
+    merge_chunks(p, ll_node, true);
   }
 
-  // otherwise, just append.
+  // non-contiguous; append ll_node
   else {
     ll_node->next = p->next;
     p->next = ll_node;

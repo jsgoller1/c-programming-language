06900bc717caf2152543a3fa8522544fec7cb859
Joshua Goller <joshua.goller@uber.com>
2018-06-15 23:11:18 -0700

    working on seek

--- ch-8/fopen_j/src/main.c
+++ ch-8/fopen_j/src/main.c
@@ -10,7 +10,7 @@
 
 int main() {
   char string1[] = "abcdefghijklmnopqrstuvwxyz\0";
-  // char string2[] = "124567890";
+  char string2[] = "AAAAAAAAAAAAAAAAAAAAAAAAAA\0";
   // char result[100];
 
   char filename[] = "test.txt";
@@ -25,17 +25,13 @@ int main() {
     printf("writing %c\n", string1[i]);
     putc_j(file, string1[i]);
   }
-  // print_buffer(file);
-  fclose_j(file);
 
-  /*
-  file = fopen_j(filename, &mode);
+  fseek_j(file, 0, SEEK_SET);
 
-  for (int i = 0; i < (int)strlen(string1); i++) {
-    result[i] = getc_j(string1[i], file);
+  for (int i = 0; i < (int)strlen(string2); i++) {
+    printf("writing %c\n", string2[i]);
+    putc_j(file, string2[i]);
   }
 
-  getc_j(contents1, 1, 10, file);
   fclose_j(file);
-*/
 }

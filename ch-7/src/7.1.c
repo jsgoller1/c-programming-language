cb3889c5dc43c8697f2576454031749da0efc675
Joshua Goller <joshua.goller@uber.com>
2018-06-18 10:59:39 -0700

    7.1 initial commit, need to reverse flags

--- ch-7/src/7.1.c
+++ ch-7/src/7.1.c
@@ -1,4 +1,6 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 /* Write a program that converts upper case to lower
  * or lower case to upper, depending on the name it
@@ -7,17 +9,24 @@
 
 int main(int argc, char** argv) {
   if (argc < 1) {
-    print("usage: caser -[ul] <string>");
+    printf("usage: caser -[ul] <string>");
     return -1;
   }
 
-  if (strcmp("-u", argv[1]){
-    // TODO: convert to uppercase
-  } else if (strcmp("-l", argv[1]){
-    // TODO: convert to lowercase
+  char* case_flag = argv[1];
+  char* in_string = argv[2];
+
+  if (strcmp("-u", case_flag)) {
+    for (size_t i = 0; i < strlen(in_string); i++) {
+      putchar(toupper(in_string[i]));
+    }
+  } else if (strcmp("-l", case_flag)) {
+    for (size_t i = 0; i < strlen(in_string); i++) {
+      putchar(tolower(in_string[i]));
+    }
   } else {
-    print("usage: caser -[ul] <string>");
+    printf("usage: caser -[ul] <string>");
     return -1;
   }
-    return 0;
+  return 0;
 }

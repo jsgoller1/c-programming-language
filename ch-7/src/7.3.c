1ddd6d37b8c59b19922b41ac1c684f85b690abc7
Joshua Goller <jsgoller1@gmail.com>
2018-06-20 02:08:26 -0700

    7.3

---
+++ ch-7/src/7.3.c
@@ -0,0 +1,56 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/*
+ * Revise minprintf to handle more of the other facilities of printf()
+ */
+
+// minprintf(): minimal printf with variable argument list
+static void minprintf(char* fmt, ...) {
+  // va_list points to each unnamed arg in turn
+  va_list ap;
+  char *p, *sval;
+  int ival;
+  double dval;
+
+  // make ap point to first unnamed arg
+  va_start(ap, fmt);
+  for (p = fmt; *p; p++) {
+    if (*p != '%') {
+      putchar(*p);
+      continue;
+    }
+    switch (*++p) {
+      case 'd':
+        ival = va_arg(ap, int);
+        printf("%d", ival);
+        break;
+      case 'f':
+        dval = va_arg(ap, double);
+        printf("%f", dval);
+        break;
+      case 's':
+        for (sval = va_arg(ap, char*); *sval; sval++) {
+          putchar(*sval);
+        }
+        break;
+      case 'o':
+        ival = va_arg(ap, int);
+        printf("%o", ival);
+        break;
+      case 'x':
+        ival = va_arg(ap, int);
+        printf("%x", ival);
+        break;
+      default:
+        putchar(*p);
+        break;
+    }
+  }
+  va_end(ap);  // clean up when done
+}
+
+int main() {
+  minprintf("%d %f %s %o %x\n", 10, 20.5, "foobarbaz", 999, 255);
+  return 0;
+}

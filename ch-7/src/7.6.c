9766a8be5fc6de993aa6df1abc1bf16bfc9e2be0
Joshua Goller <joshua.goller@uber.com>
2018-06-24 16:07:42 -0700

    7.6 seems solved.

--- ch-7/src/7.6.c
+++ ch-7/src/7.6.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
  * Ex. 7.6: Write a program to compare two files, printing the first line where
@@ -16,13 +17,15 @@ int main(int argc, char** argv) {
   // Open files for reading
   FILE* f1 = NULL;
   FILE* f2 = NULL;
-  if ((f1 = fopen(argv[0], 'r') == NULL) {
+  if ((f1 = fopen(argv[1], "r")) == NULL) {
     printf("Cannot open %s.\n", argv[0]);
+    perror("");
     return -1;
   }
 
-  if ((f2 = fopen(argv[1], 'r') == NULL) {
+  if ((f2 = fopen(argv[2], "r")) == NULL) {
     printf("Cannot open %s.\n", argv[1]);
+    perror("");
     return -1;
   }
 
@@ -30,23 +33,29 @@ int main(int argc, char** argv) {
   // and strcmp() the results. They are only
   // the same if strcmp() never returns nonzero
   // and they both EOF simultaneously.
-  char read1[100];
-  char read2[100];
+  char read1[READ_SIZE] = {0};
+  char read2[READ_SIZE] = {0};
+  char* ret1;
+  char* ret2;
   int cmp = 0;
-  int line = 1;
+  int line = 0;
   do {
-    read1 = fgets(read1, READ_SIZE, f1);
-    read2 = fgets(read2, READ_SIZE, f2);
-    cmp == strcmp(read1, read2);
+    ret1 = fgets(read1, READ_SIZE, f1);
+    ret2 = fgets(read2, READ_SIZE, f2);
+    cmp = strcmp(read1, read2);
     line++;
-  } while (cmp == 0 && !(read1 == NULL || read2 == NULL));
+  } while (cmp == 0 && !(ret1 == NULL || ret2 == NULL));
 
   // Determine result based on exit condtions
-  if (cmp != 0 || !(read1 == NULL && read2 == NULL)) {
-    printf("Files differ on line %d:\n%s: %s\n%s: %s\n", line, argv[1], read1,
+  if (ret1 != NULL && ret2 == NULL) {
+    printf("%s is longer than %s\n", argv[1], argv[2]);
+  } else if (ret1 == NULL && ret2 != NULL) {
+    printf("%s is longer than %s\n", argv[2], argv[1]);
+  } else if (cmp != 0) {
+    printf("Files differ on line %d:\n%s: %s%s: %s", line, argv[1], read1,
            argv[2], read2);
   } else {
-    printf("Files %s and %s are the same", argv[1], argv[2]);
+    printf("Files %s and %s are the same.\n", argv[1], argv[2]);
   }
 
   return 0;

b1b4323e256e05d1c8ff137879728cc29b65ba20
Joshua Goller <joshua.goller@uber.com>
2018-06-25 11:00:35 -0700

    working through 6.3 compiler bugs

--- ch-6/src/6.3.c
+++ ch-6/src/6.3.c
@@ -16,8 +16,7 @@
  *
  * I will abuse alloca() to make this easier on myself.
  */
-
-#define MAXWORD 100
+#define MAX_WORD 100
 #define INIT_LINES 25
 
 typedef struct word_node {
@@ -29,23 +28,23 @@ typedef struct word_node {
   struct word_node* right;
 } word_node;
 
-static word_node* create_node(const char* const word) {
+static word_node* create_node(char* word) {
   word_node* new_node = malloc(sizeof(word_node));
 
   // set up string
-  int word_len = strlen(word);
-  new_node->string = malloc(word_len + 1);
-  if (new_node->string == NULL) {
+  int word_len = (int)strlen(word);
+  new_node->word = malloc((unsigned long)(word_len + 1));
+  if (new_node->word == NULL) {
     free(new_node);
     return NULL;
   }
-  strcpy(new_node->string, word);
-  new_node->string[word_len] = '\0';
+  strcpy(new_node->word, word);
+  new_node->word[word_len] = '\0';
 
   // set up lines array
   new_node->lines = malloc(INIT_LINES);
   if (new_node->lines == NULL) {
-    free(new_node->string);
+    free(new_node->word);
     free(new_node);
     return NULL;
   }
@@ -59,20 +58,21 @@ static word_node* create_node(const char* const word) {
   return new_node;
 }
 
-static word_node* tree_insert(const char* const word,
-                              const word_node* const node) {
+static word_node* tree_insert(char* word, word_node* node) {
   int result = strcmp(node->word, word);
   // Word is already present in tree
   if (result == 0) {
-    return head;
-  } else if (result < 0) {
+    return node;
+  }  // left subtree
+  else if (result < 0) {
     if (node->left == NULL) {
       node->left = create_node(word);
       return node->left;
     } else {
       tree_insert(word, node->left);
     }
-  } else if (result > 0) {
+  }  // right subtree
+  else {
     if (node->right == NULL) {
       node->right = create_node(word);
       return node->right;
@@ -80,10 +80,11 @@ static word_node* tree_insert(const char* const word,
       tree_insert(word, node->right);
     }
   }
+  // Should never reach here; silence compiler error
+  return NULL;
 }
 
-static word_node* tree_search(const char* const word,
-                              const word_node* const node) {
+static word_node* tree_search(char* word, word_node* node) {
   // if the node isn't present in the tree
   if (node == NULL) {
     return NULL;
@@ -92,17 +93,24 @@ static word_node* tree_search(const char* const word,
   // walk tree based on comparison to word
   int result = strcmp(node->word, word);
   if (result == 0) {
-    return head;
+    return node;
   } else if (result < 0) {
     tree_search(word, node->left);
   } else if (result > 0) {
     tree_search(word, node->right);
   }
+
+  // Node not present in tree
+  return NULL;
 }
 
 static void tree_walk(word_node* head) {
+  if (head == NULL) {
+    return;
+  }
+
   tree_walk(head->left);
-  printf("%s\n", head->string);
+  printf("%s\n", head->word);
   tree_walk(head->right);
 }
 
@@ -112,7 +120,7 @@ static void tree_cleanup(word_node* head) {
   } else if (head->right != NULL) {
     tree_cleanup(head->right);
   } else {
-    free(head->string);
+    free(head->word);
     free(head->lines);
     free(head);
   }
@@ -128,7 +136,7 @@ static int resize_line_arr(word_node* node) {
   // resize old array to 2x
   node->lines_max *= 2;
   free(node->lines);
-  node->lines = malloc(node->lines_max * 2);
+  node->lines = malloc((unsigned long)(node->lines_max * 2));
 
   // copy old chars to new array
   for (int i = 0; i < node->lines_n; i++) {
@@ -139,7 +147,7 @@ static int resize_line_arr(word_node* node) {
 }
 
 static int add_line(word_node* node, int line) {
-  if (!(word_node->lines_n < word_node->lines_max)) {
+  if (!((word_node->lines_n) < (word_node->lines_max))) {
     if (resize_line_arr(node) == -1) {
       return -1;
     }
@@ -148,12 +156,30 @@ static int add_line(word_node* node, int line) {
   return 0;
 }
 
+/*
+static int is_noise_word( char*  word, word_node* noise_words) {
+  if (tree_search(word, noise_words) != NULL) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+*/
+
 int main() {
   char word[MAX_WORD];
-  word_node* head;
-  word_node* current;
+  word_node* noise_words = NULL;
+  word_node* head = NULL;
+  word_node* current = NULL;
 
   while (getword(word) != EOF) {
+    /*
+    // ignore noise words
+    if (is_noise_word(word, noise_words)) {
+      continue;
+    }
+    */
+
     // see if we've encountered the word before
     if ((current = tree_search(word)) == NULL) {
       current = tree_insert(word);

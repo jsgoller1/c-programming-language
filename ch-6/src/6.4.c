03da7ada9809b55d27a2994d5bda2f6b76981893
Joshua Goller <joshua.goller@uber.com>
2018-06-28 22:14:09 -0700

    hopefully fully fleshed out 6.4

--- ch-6/src/6.4.c
+++ ch-6/src/6.4.c
@@ -25,11 +25,70 @@ typedef struct word_count {
   int count;
 } word_count;
 
-static void display_word_count() {}
+static tnode* word_tree = NULL;
+static tnode* count_tree = NULL;
 
-static word_count* word_count_alloc() {}
+static void result_print(const tnode* const node) {
+  printf("%s: %ul\n", node->data->word, node->data->count);
+}
 
-static word_count* word_count_free() {}
+static int count_cmp(const tnode* const node1, const tnode* const node2) {
+  int count1 = node1->data->count;
+  int count2 = node2->data->count;
+  if (count1 == count2) {
+    return 0;
+  } else if (count1 < count2) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+static int word_cmp(const tnode* const node1, const tnode* const node2) {
+  char* word1 = node1->data->word;
+  char* word2 = node2->data->word;
+  return strcmp(word1, word2);
+}
+
+static word_count* word_count_alloc(const char* const word) {
+  word_count* wc = NULL;
+  char* string = NULL;
+  int len = strlen(word);
+
+  wc = malloc(sizeof(word_count));
+  if (wc == NULL) {
+    return NULL;
+  }
+
+  wc->word = malloc(len * sizeof(char));
+  if (wc->word == NULL) {
+    free(wc);
+    return NULL;
+  }
+
+  strcpy(wc->word, word);
+  return wc;
+}
+
+static void word_count_free(const word_count* const wc) {
+  if (wc == NULL) {
+    return;
+  }
+
+  free(wc->word);
+  free(wc);
+}
+
+static void word_count_extractor(const tnode* const node) {
+  size_t count = count_tree->data->count;
+  tree_insert(count_tree, count, sizeof(count), intcmp);
+}
+
+static word_count* create_count_tree(const tnode* const word_tree) {
+  tnode* count_tree = NULL;
+  trav_inorder(word_tree, word_count_extractor);
+  return count_tree;
+}
 
 static tnode* create_word_tree() {
   int i;
@@ -53,15 +112,14 @@ static tnode* create_word_tree() {
       tree = tree_insert(tree, wdc, i, strcmp);
     }
   }
+
+  return tree;
 }
 
 int main() {
-  tnode* word_tree = NULL;
-  tnode* count_tree = NULL;
-
   word_tree = create_word_tree();
-  count_tree = create_count_tree(word_tree);
-  trav_preorder(count_tree);
+  create_count_tree(word_tree);
+  trav_preorder(count_tree, result_print);
 
   return 0;
 }

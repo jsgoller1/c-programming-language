094e9e91c0f379cbbe757c4b069b6082fdf8a514
Joshua Goller <jsgoller1@gmail.com>
2018-07-07 21:40:25 -0700

    finished transcribing decl

---
+++ ch-5/decl/src/gettoken.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "decl.h"
+
+int gettoken(void) {
+  char *p = token;
+
+  while ((c = getch()) == ' ' || c == '\t') {
+    // skip whitespaces
+  }
+
+  if (c == '(') {
+    if ((c = getch()) == ')') {
+      strcpy(token, "()");
+      return tokentype = PARENS;
+    } else {
+      ungetch(c);
+      return tokentype = '(';
+    }
+  } else if (c == '[') {
+    for (*p++ = c; (*p++ = getch()) != ']';) {
+      // skip array size specifier
+    }
+    *p = '\0';
+    return tokentype = BRACKETS;
+  } else if (isalpha(c)) {
+    for (*p++ = c; isalnum(c = getch());) {
+      *p++ = c;
+    }
+    *p = '\0';
+    ungetch(c);
+    return tokentype = NAME;
+  } else {
+    return tokentype = c;
+  }
+}
--- ch-5/decl/src/main.c
+++ ch-5/decl/src/main.c
@@ -3,5 +3,19 @@
 #include "decl.h"
 
 char token[MAXTOKEN] = {0};
+char name[MAXTOKEN] = {0};
+char datatype[MAXTOKEN] = {0};
+char out[MAXTOKEN] = {0};
 
-int main() {}
+int main() {
+  while (gettoken() != EOF) {
+    strcpy(datatype, token);
+    out[0] = '\0';
+    decl();
+    if (tokentype != '\n') {
+      printf("syntax error.\n");
+    }
+    printf("%s: %s %s\n", name, out, datatype);
+  }
+  return 0;
+}

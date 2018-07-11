16f62c6a515fca4b1a2e94d5b3ae41bbc448e07f
Joshua Goller <joshua.goller@uber.com>
2018-07-11 11:59:55 -0700

    confirming shorthand produces correct results

--- ch-5/tabber/tabber-test.sh
+++ ch-5/tabber/tabber-test.sh
@@ -2,8 +2,9 @@
 
 #./bin/tabber
 #printf "a         b" | ./bin/tabber -b entab
-printf "a\t\tb" | ./bin/tabber -b detab
+#printf "a\t\tb" | ./bin/tabber -b detab
 #./bin/tabber -b detab -l 0 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75
 #./bin/tabber -b entab -l 0 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75
-#./bin/tabber -b detab -m 10+5
+printf "a         b" | ./bin/tabber -b entab -m 5+10
+#printf "a\t\tb" | ./bin/tabber -b detab -m 5+10
 #./bin/tabber -b detab -m 10+5 -l 10 15 20

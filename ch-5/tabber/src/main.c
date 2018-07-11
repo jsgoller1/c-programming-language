a4eb9c5921da7683d568b0ba03c7bc4008af2888
Joshua Goller <joshua.goller@uber.com>
2018-07-11 01:40:42 -0700

    clean up main()

--- ch-5/tabber/src/main.c
+++ ch-5/tabber/src/main.c
@@ -10,19 +10,19 @@ int main(int argc, char** argv) {
   char* processed_line = NULL;  // line after entabing / detabing
   int* tab_stops = NULL;        // list of tab stops
   int tab_stops_len = 0;
-
-  int input = 0;
   char* (*behavior)(const char* const, const int, const int* const,
                     const int);  // detab or entab, depending on flags
 
   // handle input
-  if ((input = parse_flags(argc, argv, &tab_stops, &tab_stops_len)) ==
-      USE_ENTAB) {
-    behavior = entab;
-  } else if (input == USE_DETAB) {
-    behavior = detab;
-  } else {
-    return -1;
+  switch (parse_flags(argc, argv, &tab_stops, &tab_stops_len)) {
+    case USE_ENTAB:
+      behavior = entab;
+      break;
+    case USE_DETAB:
+      behavior = detab;
+      break;
+    default:
+      return -1;
   }
 
   // Testing: input handling
@@ -32,20 +32,12 @@ int main(int argc, char** argv) {
   }
   printf("\n");
 
-  /*
-  // Testing: detab-ing / entabing
-  (void)behavior;
-  tab_stops[] = {0, 5, 10, 15, 20, 25, 30, 35};
-  tab_stops_len = 8;
-
   // do the thing
   while ((len = mygetline(line, MAXLEN)) > 0) {
-    // processed_line = behavior(line, len, tab_stops);
-    processed_line = entab(line, len, tab_stops, tab_stops_len);
+    processed_line = behavior(line, len, tab_stops, tab_stops_len);
     printf("%s\n", processed_line);
     free(processed_line);
   }
-  */
 
   free(tab_stops);
   return 0;
--- ch-5/tabber/tabber-test.sh
+++ ch-5/tabber/tabber-test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #./bin/tabber
-#./bin/tabber -b entab
+./bin/tabber -b entab
 #./bin/tabber -b detab
-#./bin/tabber -b detab -l 0 1 1 2 3 5 8 13 21
-./bin/tabber -b detab -m 10+5
-./bin/tabber -b detab -m 10+5 -l 10 15 20
+#./bin/tabber -b detab -l 0 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75
+#./bin/tabber -b entab -l 0 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75
+#./bin/tabber -b detab -m 10+5
+#./bin/tabber -b detab -m 10+5 -l 10 15 20

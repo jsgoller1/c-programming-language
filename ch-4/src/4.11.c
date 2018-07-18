18d7c7a16dd07daa0efaab3f7a445768e0f42518
Joshua Goller <joshua.goller@uber.com>
2018-07-18 02:30:04 -0400

    4.11 compiles now

--- ch-4/src/4.11.c
+++ ch-4/src/4.11.c
@@ -5,16 +5,61 @@
 // Ex. 4.11 - Modify getop so that it doesn't need to use ungetch. Hint: use an
 // internal static variable.
 
-#define MAXOP 100  // max size for operand or operator
+#define MAXOP 100   // max size for operand or operator
+#define MAXVAL 100  // maximum depth of val stack
 #define NUMBER '0'
-
 static int c = ' ';
 
-int getch(void);
-void ungetch(int);
-int getop(char[]);
-void push(double);
-double pop(void);
+static int sp = 0;
+static double val[MAXVAL];
+
+// push() - push f onto value stack
+static void push(const double f) {
+  if (sp < MAXVAL) {
+    val[sp++] = f;
+  } else {
+    printf("Error; stack full - can't push %g\n", f);
+  }
+}
+
+// pop: pop and return top value from stack
+static double pop() {
+  if (sp > 0) {
+    return val[--sp];
+  } else {
+    printf("Error: stack empty\n");
+    return 0.0;
+  }
+}
+
+// getop: get next operator or numeric operand
+static int getop(char* const s) {
+  int i;
+
+  while (((s[0] = (char)c) == ' ') || (c == '\t')) {
+    // no op; get all characters until a space/tab/newline is hit
+    c = getchar();
+  }
+  s[1] = '\0';
+  if (!isdigit(c) && c != '.') {
+    return c;  // if not a number or floating point
+  }
+  i = 0;
+  if (isdigit(c))  // collect integer part
+  {
+    while (isdigit(s[++i] = c = (char)getchar())) {
+      // no-op
+    }
+  }
+  if (c == '.')  // collect fractional part
+  {
+    while (isdigit(s[++i] = c = (char)getchar())) {
+      // no-op
+    }
+  }
+  s[i] = '\0';
+  return NUMBER;
+}
 
 // Reverse Polish calculator
 int main() {
@@ -60,62 +105,4 @@ int main() {
     }
   }
   return 0;
-}
-
-#define MAXVAL 100  // maximum depth of val stack
-
-static int sp = 0;
-static double val[MAXVAL];
-
-// push() - push f onto value stack
-void push(double f) {
-  if (sp < MAXVAL) {
-    val[sp++] = f;
-  } else {
-    printf("Error; stack full - can't push %g\n", f);
-  }
-}
-
-// pop: pop and return top value from stack
-double pop() {
-  if (sp > 0) {
-    return val[--sp];
-  } else {
-    printf("Error: stack empty\n");
-    return 0.0;
-  }
-}
-
-// getop: get next operator or numeric operand
-int getop(char s[]) {
-  int i;
-
-  while (((s[0] = (char)c) == ' ') || (c == '\t')) {
-    // no op; get all characters until a space/tab/newline is hit
-    c = getchar();
-  }
-  s[1] = '\0';
-  if (!isdigit(c) && c != '.') {
-    return c;  // if not a number or floating point
-  }
-  i = 0;
-  if (isdigit(c))  // collect integer part
-  {
-    while (isdigit(s[++i] = c = (char)getchar())) {
-      // no-op
-    }
-  }
-  if (c == '.')  // collect fractional part
-  {
-    while (isdigit(s[++i] = c = (char)getchar())) {
-      // no-op
-    }
-  }
-  s[i] = '\0';
-  /*if (c != EOF)
-  {
-      ungetch(c);
-  }
-  */
-  return NUMBER;
 }

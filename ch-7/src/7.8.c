04f3bd4966b90e3b9a7275a0455c1b12ed98ea34
Joshua Goller <joshua.goller@uber.com>
2018-07-20 00:54:30 -0400

    7.8

--- ch-7/src/7.8.c
+++ ch-7/src/7.8.c
@@ -3,8 +3,6 @@
 /*
  * Exercise 7-8. Write a program to print a set of files, starting each new one
  * on a new page, with a title and a running page count for each file.
- * ---------------------
- *
  */
 
 #define PAGE_LEN 40

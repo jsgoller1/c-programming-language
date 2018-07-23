dc856cd988fe42f233e5877d90c4915b835733be
Joshua Goller <joshua.goller@uber.com>
2018-07-23 15:12:00 -0400

    remove unnecessary headers

--- ch-5/include/5.11a.h
+++
@@ -1,4 +0,0 @@
-char* entab(const char* const in_line, const int in_len, const int* tab_stops);
-int look_ahead(const char* const in_line, const int in_len, int offset,
-               const int tab_stop);
-int get_next_tabstop(const int* const tab_stops, const int current_char);
--- ch-5/include/5.8-9.h
+++
@@ -1,2 +0,0 @@
-int day_of_year(const int year, const int month, int day);
-int month_day(const int year, int yearday, int *const pmonth, int *const pday);

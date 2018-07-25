5e11297309960f3922bea7633586bcda3661dbfb
Joshua Goller <joshua.goller@uber.com>
2018-07-24 20:36:59 -0400

    clean up undecl test

--- ch-5/decl/undecl-test.sh
+++ ch-5/decl/undecl-test.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
 # char* x()
+echo "In: x () * char"
+echo "Expected:  char* x()"
+printf "Actual: "
 echo "x () * char" | ./bin/undecl | grep "char\* x()"
+printf "\n"
 
 # char x[]
+echo "In: x [] char"
+echo "Expected:  char x[]"
+printf "Actual: "
 echo "x [] char" | ./bin/undecl | grep "char x\[\]"
+printf "\n"
 
 # char (*x)()
 # declare x as pointer to function returning char
+echo "In: x * () char"
+echo "Expected: char (*x)()"
+printf "Actual: "
 echo "x * () char" | ./bin/undecl | grep "char (\*x)()"
+printf "\n"
 
 # char (*(*x())[])()
 # declare x as function returning pointer to array of pointer to function returning char
+echo "In: x () * [] * () char"
+echo "Expected: char (*(*x())[])()"
+printf "Actual: "
 echo "x () * [] * () char" | ./bin/undecl | grep "char (\*(\*x())\[\])()"
+printf "\n"
 
 # char** (*x)()
 # declare x as pointer to function returning pointer to pointer to char
+echo "In: x * () * * char"
+echo "Expected:  char **(*x)()"
+printf "Actual: "
 echo "x * () * * char" | ./bin/undecl | grep "char \*\*(\*x)()"
+printf "\n"
